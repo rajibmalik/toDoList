b3b4cc3a1dfb8b65ce3a8529ed6811c5b1cc3ae5
rajibmalik <rajibmalik@hotmail.co.uk>
2025-01-06 16:09:48 +0000

    Completed todos rendered correctly

--- src/dom/createToDoDOM.js
+++ src/dom/createToDoDOM.js
@@ -1,6 +1,30 @@
 import { format } from "date-fns";
 
 export const createToDoDOM = (function () {
+  function create(toDo) {
+    const container = createContainer(toDo.getId());
+    const checkBox = createCheckBox(toDo.getId(), toDo.getIsComplete());
+    const title = createTitle(toDo.getTitle(), toDo.getIsComplete());
+    const informationContainer = createInformationContainer();
+    const descriptionBtn = createDescriptionBtn(toDo.getIsComplete());
+    const dueDate = createDueDate(toDo.getDueDate(), toDo.getIsComplete());
+    const editAndDeleteBtnContainer = createEditAndDeleteBtnContainer();
+    const editBtn = createEditButton(toDo.getIsComplete());
+    const deleteBtn = createDeleteButton(toDo.getIsComplete());
+
+    container.appendChild(checkBox);
+    container.appendChild(title);
+    informationContainer.appendChild(descriptionBtn);
+    informationContainer.appendChild(dueDate);
+    editAndDeleteBtnContainer.appendChild(editBtn);
+    editAndDeleteBtnContainer.appendChild(deleteBtn);
+
+    container.appendChild(informationContainer);
+    container.appendChild(editAndDeleteBtnContainer);
+
+    return container;
+  }
+
   function createContainer(id) {
     const container = document.createElement("div");
     container.classList.add("toDoContainer");
@@ -9,13 +33,16 @@ export const createToDoDOM = (function () {
     return container;
   }
 
-  function createCheckBox(id) {
+  function createCheckBox(id, isComplete) {
     const checkBoxContainer = document.createElement("label");
     checkBoxContainer.classList.add("toDoCheckBoxContainer");
     checkBoxContainer.setAttribute("for", id);
 
     const checkBox = document.createElement("input");
     checkBox.classList.add("toDoCheckBox");
+    if (isComplete) {
+      checkBox.checked = true;
+    }
     checkBox.id = id;
     checkBox.type = "checkbox";
 
@@ -28,12 +55,15 @@ export const createToDoDOM = (function () {
     return checkBoxContainer;
   }
 
-  function createTitle(title) {
+  function createTitle(title, isComplete) {
     const titleElementContainer = document.createElement("div");
     titleElementContainer.classList.add("toDoTitleContainer");
 
     const titleElement = document.createElement("p");
     titleElement.classList.add("toDoTitle");
+    if (isComplete) {
+      titleElement.classList.add("completed");
+    }
     titleElement.textContent = title;
 
     titleElementContainer.appendChild(titleElement);
@@ -48,12 +78,15 @@ export const createToDoDOM = (function () {
     return container;
   }
 
-  function createDescriptionBtn() {
+  function createDescriptionBtn(isComplete) {
     const descriptionBtnContainer = document.createElement("div");
     descriptionBtnContainer.classList.add("toDoDescriptionBtnContainer");
 
     const descriptionBtn = document.createElement("button");
     descriptionBtn.classList.add("toDoDescriptionBtn");
+    if (isComplete) {
+      descriptionBtn.classList.add("completed");
+    }
     descriptionBtn.textContent = "DETAILS";
 
     descriptionBtnContainer.appendChild(descriptionBtn);
@@ -61,7 +94,7 @@ export const createToDoDOM = (function () {
     return descriptionBtnContainer;
   }
 
-  function createDueDate(dueDate) {
+  function createDueDate(dueDate, isComplete) {
     const dateObject = new Date(dueDate);
     console.log("date: " + dueDate);
     console.log("dateObject: " + dateObject);
@@ -74,6 +107,9 @@ export const createToDoDOM = (function () {
     const date = document.createElement("p");
     date.textContent = formattedDate;
     date.classList.add("toDoDate");
+    if (isComplete) {
+      date.classList.add("completed");
+    }
 
     dateContainer.appendChild(date);
 
@@ -99,25 +135,31 @@ export const createToDoDOM = (function () {
     return container;
   }
 
-  function createEditButton() {
+  function createEditButton(isComplete) {
     const editButtonContainer = document.createElement("div");
     editButtonContainer.classList.add("toDoEditBtnContainer");
 
     const editButton = document.createElement("button");
     editButton.classList.add("toDoEditBtn");
+    if (isComplete) {
+      editButton.classList.add("completed");
+    }
 
     editButtonContainer.appendChild(editButton);
 
     return editButtonContainer;
   }
 
-  function createDeleteButton() {
+  function createDeleteButton(isComplete) {
     const deleteButtonContainer = document.createElement("div");
     deleteButtonContainer.classList.add("toDoDeleteBtnContainer");
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("fa-solid", "fa-trash");
     deleteButton.classList.add("toDoDeleteBtn");
+    if (isComplete) {
+      deleteButton.classList.add("completed");
+    }
 
     deleteButtonContainer.appendChild(deleteButton);
 
@@ -125,6 +167,7 @@ export const createToDoDOM = (function () {
   }
 
   return {
+    create,
     createContainer,
     createCheckBox,
     createTitle,
--- src/renders/renderToDo.js
+++ src/renders/renderToDo.js
@@ -8,78 +8,9 @@ export const renderToDo = (function () {
   const content = document.querySelector("#toDoList");
 
   function render(toDo) {
-    const container = createToDoDOM.createContainer(toDo.getId());
-    const checkBox = createCheckBox(toDo.getId());
-    const title = createTitle(toDo.getTitle());
-    const informationContainer = createInformationContainer();
-    const descriptionBtn = createDescriptionBtn(toDo.getDescription());
-    const dueDate = createDueDate(toDo.getDueDate());
-    const editAndDeleteBtnContainer = createEditAndDeleteBtnContainer();
-    const editBtn = createEditBtn();
-    const deleteBtn = createDeleteBtn();
+    const toDoDOMElement = createToDoDOM.create(toDo);
 
-    container.appendChild(checkBox);
-    container.appendChild(title);
-    informationContainer.appendChild(descriptionBtn);
-    informationContainer.appendChild(dueDate);
-    editAndDeleteBtnContainer.appendChild(editBtn);
-    editAndDeleteBtnContainer.appendChild(deleteBtn);
-
-    container.appendChild(informationContainer);
-    container.appendChild(editAndDeleteBtnContainer);
-
-    content.appendChild(container);
-  }
-
-  function createCheckBox(id) {
-    const toDoElement = createToDoDOM.createCheckBox(id);
-    // checkBoxEventListener.createListener();
-
-    return toDoElement;
-  }
-
-  function createTitle(title) {
-    const titleElement = createToDoDOM.createTitle(title);
-
-    return titleElement;
-  }
-
-  function createInformationContainer() {
-    const container = createToDoDOM.createInformationContainer();
-
-    return container;
-  }
-
-  function createDescriptionBtn(description) {
-    const btnElement = createToDoDOM.createDescriptionBtn();
-
-    return btnElement;
-  }
-
-  function createDueDate(date) {
-    const dateElement = createToDoDOM.createDueDate(date);
-
-    return dateElement;
-  }
-
-  function createEditAndDeleteBtnContainer() {
-    const container = createToDoDOM.createEditAndDeleteBtnContainer();
-
-    return container;
-  }
-
-  function createEditBtn() {
-    const btnElement = createToDoDOM.createEditButton();
-    editBtnClickListener.createListener(btnElement);
-
-    return btnElement;
-  }
-
-  function createDeleteBtn() {
-    const btnElement = createToDoDOM.createDeleteButton();
-    deleteBtnClickListener.createListener(btnElement);
-
-    return btnElement;
+    content.appendChild(toDoDOMElement);
   }
 
   return { render };
